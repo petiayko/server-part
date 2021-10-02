c79a6d630c2cddd0d0a3ab9f8aa87fd8b221dbd7
Peter <p.makretskii@gmail.com>
2021-10-02 18:44:56 +0300

    v1.7 jwt

--- database_requests.py
+++ database_requests.py
@@ -1,8 +1,30 @@
 import json
-from flask import request
+from flask import request, jsonify
 import database
 from db_init import app, user_db, Users
 import jwt
+import datetime
+from functools import wraps
+
+
+def token_required(req):
+    @wraps(req)
+    def decorated(*args, **kwargs):
+        #
+        # if 'Authorization' in request.headers:
+        #     token = request.headers['Authorization']
+        #     print(token)
+        # else:
+        #     return app.make_response(('Token is wrong', 403))
+        token = request.headers['Authorization']
+        token = token[token.find('": "')+4:token.find('"\\')]
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+        except Exception as e:
+            print(e)
+            return app.make_response(('Token is wrong', 403))
+        return req(*args, **kwargs)
+    return decorated
 
 
 @app.route('/auth/', methods=['GET'])
@@ -11,21 +33,30 @@ def auth():
         login = request.args['login']
         password = request.args['password']
         if database.auth(login=login, password=password):
-            pass
+            token = jwt.encode({'user': login,
+                                'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=15)
+                                }, app.config['SECRET_KEY'])
+            return jsonify({'token': token.decode('UTF-8')})
+        return 'denied'
 
 
-@app.route('/check_email/', methods=['GET'])
-def check_email():
-    if request.method == 'GET':
-        email = request.args['email']
-        return str(user_db.session.query(Users).filter_by(mail=email).count() != 0)
+# @app.route('/check_email/', methods=['GET'])
+# def check_email():
+#     if request.method == 'GET':
+#         email = request.args['email']
+#         return str(user_db.session.query(Users).filter_by(mail=email).count() != 0)
 
 
-@app.route('/check_login/', methods=['GET'])
-def check_login():
-    if request.method == 'GET':
-        login = request.args['login']
-        return str(user_db.session.query(Users).filter_by(login=login).count() != 0)
+# @app.route('/check_login/', methods=['GET'])
+# def check_login():
+#     if request.method == 'GET':
+#         login = request.args['login']
+#         return str(user_db.session.query(Users).filter_by(login=login).count() != 0)
+
+@app.route('/test/')
+@token_required
+def test():
+    return 'True'
 
 
 @app.route('/get_password/', methods=['GET'])
@@ -56,6 +87,7 @@ def add_user():
 
 
 @app.route('/delete_user/', methods=['GET'])
+@token_required
 def delete_user():
     if request.method == 'GET':
         database.delete_user(login=request.args['login'])
@@ -63,6 +95,7 @@ def delete_user():
 
 
 @app.route('/change_mail/', methods=['GET'])
+@token_required
 def change_mail():
     if request.method == 'GET':
         login = request.args['login']
@@ -72,6 +105,7 @@ def change_mail():
 
 
 @app.route('/change_password/', methods=['GET'])
+@token_required
 def change_password():
     if request.method == 'GET':
         login = request.args['login']
@@ -81,6 +115,7 @@ def change_password():
 
 
 @app.route('/add_version/', methods=['GET'])
+@token_required
 def add_version():
     if request.method == 'GET':
         files = json.loads(request.data.decode('UTF-8'))
@@ -107,6 +142,7 @@ def add_version():
 
 
 @app.route('/update_version/', methods=['GET'])
+@token_required
 def update_version():
     if request.method == 'GET':
         files = json.loads(request.data.decode('UTF-8'))
@@ -140,6 +176,7 @@ def update_version():
 
 
 @app.route('/delete_version/', methods=['GET'])
+@token_required
 def delete_version():
     if request.method == 'GET':
         database.delete_folder(
